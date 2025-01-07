3e7014d826327cb0a6627d6bef42629f4890d123
blackgauss <81921810+blackgauss@users.noreply.github.com>
2025-01-07 09:00:39 -0800

    MVP
    im already using this to journal!

--- app.py
+++ app.py
@@ -117,4 +117,5 @@ def entries():
         return jsonify({"message": "An error occurred while loading entries!"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, ssl_context=("ssl-certificates/cert.pem", "ssl-certificates/key.pem"), debug=True)
+
